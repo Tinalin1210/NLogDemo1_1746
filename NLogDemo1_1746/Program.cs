9a6c4f4071d7c9db82035e3c0502722094d8bc7a
Tina <tinalin@mail.kway.com.tw>
2025-02-11 17:44:11 +0800

    加入專案檔案。

---
+++ NLogDemo1_1746/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using NLog;
+
+namespace NLogDemo1_1746
+{
+    internal class Program
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+
+            logger.Info("這是  Info 訊息");
+
+            logger.Error("這是 Error 訊息");
+
+            Console.WriteLine("Log 寫入成功");
+            Console.ReadLine();
+        }
+    }
+}
