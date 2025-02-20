f8ba3d3ee8bdaacf440ac5075519af1732a50b72
Tina <tinalin@mail.kway.com.tw>
2025-02-20 15:22:35 +0800

    修改註解

--- NLogDemo1_1746/Program.cs
+++ NLogDemo1_1746/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
-using NLog;
+using NLog;  //引用 NLog 函式庫，讓我們能夠使用其日誌功能。
 
 namespace NLogDemo1_1746
 {
     internal class Program
     {
+        //LogManager.GetCurrentClassLogger()：取當前類別的日誌記錄器（logger）。
+        //Logger 是 NLog 提供的物件，負責記錄日誌。
+        // logger 是靜態變數，用來記錄所有的日誌訊息。
+        //readonly：只能在初始化時設置一次，並且後續不能再更改它的值。
         private static readonly Logger logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
